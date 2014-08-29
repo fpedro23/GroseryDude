1ff2f2d92617deb250e30a4c954bd8d5edef2fa1
Pedro Contreras <fpedro23@gmail.com>
2014-08-29 10:10:42 -0500

    Initial Commit

---
+++ GroceryDude/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GroceryDude
+//
+//  Created by Pedro Contreras Nava on 29/08/14.
+//  Copyright (c) 2014 Pedro Contreras Nava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GroceryDude/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GroceryDude
+//
+//  Created by Pedro Contreras Nava on 29/08/14.
+//  Copyright (c) 2014 Pedro Contreras Nava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
